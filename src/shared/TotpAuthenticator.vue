78df19692ec48eb395ebda4bf86c56e8e58ddb61
Karl <99160430+csic21@users.noreply.github.com>
2026-05-20 00:00:53 +0800

    feat: 优化账户卡片的复制按钮样式和布局

--- src/shared/TotpAuthenticator.vue
+++ src/shared/TotpAuthenticator.vue
@@ -526,32 +526,6 @@ onUnmounted(() => {
         >
           <div class="account-card__top">
             <div class="account-card__token-group">
-              <button
-                class="icon-button"
-                :disabled="
-                  !currentTokens[index]?.token ||
-                  currentTokens[index]?.token === 'Error'
-                "
-                title="Copy to clipboard"
-                @click="copyToClipboard(index, currentTokens[index]?.token)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
-                  />
-                </svg>
-                <span v-if="copiedIndex === index" class="tooltip">Copied</span>
-              </button>
-
               <div class="min-w-0">
                 <p class="token-display">
                   {{ currentTokens[index]?.token || "..." }}
@@ -565,6 +539,32 @@ onUnmounted(() => {
                 </div>
               </div>
             </div>
+
+            <button
+              class="icon-button account-card__copy-button"
+              :disabled="
+                !currentTokens[index]?.token ||
+                currentTokens[index]?.token === 'Error'
+              "
+              title="Copy to clipboard"
+              @click="copyToClipboard(index, currentTokens[index]?.token)"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                />
+              </svg>
+              <span v-if="copiedIndex === index" class="tooltip">Copied</span>
+            </button>
           </div>
 
           <div class="account-card__actions">
